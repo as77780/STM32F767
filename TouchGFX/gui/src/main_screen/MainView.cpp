f76d94601ee50262d40990f770769c9a2bb2277d
as77780 <53512513+as77780@users.noreply.github.com>
2019-11-06 18:26:23 +0300

    collor temperature and FAN

--- TouchGFX/gui/src/main_screen/MainView.cpp
+++ TouchGFX/gui/src/main_screen/MainView.cpp
@@ -35,22 +35,115 @@ void MainView::handleTickEvent()
 	    digitalClock1.setTime24Hour(presenter->getHour(), presenter->getMinute(), presenter->getSecond());
 	 	 }
  void MainView::ViewTemp() {
+	 uint8_t tempPOW=(uint8_t)presenter->getTempPow();
+	 uint8_t tempSOUND=(uint8_t)presenter->getTempSound();
+	 uint8_t FAN1=(uint8_t)presenter->getFan1();
+	 uint8_t FAN2=(uint8_t)presenter->getFan2();
 	    /*Progress bar power*/
-	    Unicode::snprintfFloat(textTempPowerBuffer,TEXTTEMPPOWER_SIZE,"%0f",presenter->getTempPow());
+	    Unicode::snprintfFloat(textTempPowerBuffer,TEXTTEMPPOWER_SIZE,"%0f",tempPOW);
 	 	textTempPower.invalidate();
-	 	lineProgressTempPow.setValue((uint8_t)presenter->getTempPow());
+	 	lineProgressTempPow.setValue(tempPOW);
 	 	lineProgressTempPow.invalidate();
 	 	  /*Progress bar sound*/
-	 	Unicode::snprintfFloat(textTempSoundBuffer,TEXTTEMPSOUND_SIZE,"%0f",presenter->getTempSound());
+	 	Unicode::snprintfFloat(textTempSoundBuffer,TEXTTEMPSOUND_SIZE,"%0f",tempSOUND);
 	 	textTempSound.invalidate();
-	 	lineProgressTempSound.setValue((uint8_t)presenter->getTempSound());
+	 	lineProgressTempSound.setValue(tempSOUND);
 	 	lineProgressTempSound.invalidate();
 	 	 /*Progress bar FAN1*/
-	 	circleProgressFAN1.setValue(presenter->getFan1());
+	 	circleProgressFAN1.setValue(FAN1);
 	 	circleProgressFAN1.invalidate();
-	 	circleProgressFAN2.setValue(presenter->getFan2());
+	 	/*Progress bar FAN2*/
+	 	circleProgressFAN2.setValue(FAN2);
 	 	circleProgressFAN2.invalidate();
-	 	 /*Progress bar FAN1*/
+/*
+ * change color Temp POW indicator
+ */
+	 	if(tempPOW<40){
+	    lineProgressTempPowPainter.setColor(touchgfx::Color::getColorFrom24BitRGB(0x00, 0x9E, 0x0D));
+	    lineProgressTempPow.setPainter(lineProgressTempPowPainter);
+        }
+
+        if((40<=tempPOW)&&(tempPOW <=50)){
+	    lineProgressTempPowPainter.setColor(touchgfx::Color::getColorFrom24BitRGB(6, 119, 196));
+	    lineProgressTempPow.setPainter(lineProgressTempPowPainter);
+	     }
+
+        if((51<=tempPOW) && (tempPOW<=60)){
+	    lineProgressTempPowPainter.setColor(touchgfx::Color::getColorFrom24BitRGB(0xb7, 0xba, 0x00));
+	    lineProgressTempPow.setPainter(lineProgressTempPowPainter);
+	     }
+
+        if(61<=tempPOW){
+	    lineProgressTempPowPainter.setColor(touchgfx::Color::getColorFrom24BitRGB(0xba,0x09, 0));
+	    lineProgressTempPow.setPainter(lineProgressTempPowPainter);
+	     }
+        /*
+         * change color Temp Sound indicator
+         */
+        if(tempSOUND<40){
+        	lineProgressTempSoundPainter.setColor(touchgfx::Color::getColorFrom24BitRGB(0x00, 0x9E, 0x0D));
+        	lineProgressTempSound.setPainter(lineProgressTempSoundPainter);
+                }
+
+        if((40<=tempSOUND) &&(tempSOUND<=50)){
+                	lineProgressTempSoundPainter.setColor(touchgfx::Color::getColorFrom24BitRGB(6, 119, 196));
+                	lineProgressTempSound.setPainter(lineProgressTempSoundPainter);
+        	     }
+
+        if((51<=tempSOUND)&&(tempSOUND<=60)){
+                	lineProgressTempSoundPainter.setColor(touchgfx::Color::getColorFrom24BitRGB(0xb7, 0xba, 0x00));
+                	lineProgressTempSound.setPainter(lineProgressTempSoundPainter);
+        	     }
+
+        if(61<=tempSOUND){
+                	lineProgressTempSoundPainter.setColor(touchgfx::Color::getColorFrom24BitRGB(0xba,0x09, 0));
+                	lineProgressTempSound.setPainter(lineProgressTempSoundPainter);
+        	     }
+                /*
+                 * change color Indicator FAN1
+                 */
+	 	if(FAN1<40){
+	 		circleProgressFAN1Painter.setColor(touchgfx::Color::getColorFrom24BitRGB(0x00, 0x9E, 0x0D));
+	 		circleProgressFAN1.setPainter(circleProgressFAN1Painter);
+        }
+
+        if((40<=FAN1)&&(FAN1 <=60)){
+        	circleProgressFAN1Painter.setColor(touchgfx::Color::getColorFrom24BitRGB(6, 119, 196));
+        	circleProgressFAN1.setPainter(circleProgressFAN1Painter);
+	     }
+
+        if((61<=FAN1) && (FAN1<=80)){
+        	circleProgressFAN1Painter.setColor(touchgfx::Color::getColorFrom24BitRGB(0xb7, 0xba, 0x00));
+        	circleProgressFAN1.setPainter(circleProgressFAN1Painter);
+	     }
+
+        if(81<=FAN1){
+        	circleProgressFAN1Painter.setColor(touchgfx::Color::getColorFrom24BitRGB(0xba,0x09, 0));
+        	circleProgressFAN1.setPainter(circleProgressFAN1Painter);
+	     }
+        /*
+                         * change color Indicator FAN2
+                         */
+        	 	if(FAN2<40){
+        	 		circleProgressFAN2Painter.setColor(touchgfx::Color::getColorFrom24BitRGB(0x00, 0x9E, 0x0D));
+        	 		circleProgressFAN2.setPainter(circleProgressFAN2Painter);
+                }
+
+                if((40<=FAN2)&&(FAN2 <=60)){
+                	circleProgressFAN2Painter.setColor(touchgfx::Color::getColorFrom24BitRGB(6, 119, 196));
+                	circleProgressFAN2.setPainter(circleProgressFAN2Painter);
+        	     }
+
+                if((61<=FAN2) && (FAN2<=80)){
+                	circleProgressFAN2Painter.setColor(touchgfx::Color::getColorFrom24BitRGB(0xb7, 0xba, 0x00));
+                	circleProgressFAN2.setPainter(circleProgressFAN2Painter);
+        	     }
+
+                if(81<=FAN2){
+                	circleProgressFAN2Painter.setColor(touchgfx::Color::getColorFrom24BitRGB(0xba,0x09, 0));
+                	circleProgressFAN2.setPainter(circleProgressFAN2Painter);
+        	     }
+
         }
  void  MainView::FunVolUP()
  {
